36bc7054fb9eda2f212202d6cfe5e62793be6b1c
Alexis <alexis.cervan@gmail.com>
2023-03-15 20:36:31 +0100

    Created parse v4, daily_reports_v4 and adapted do_daily_reports to do with v4 files

--- daily_report/do_daily_reports.py
+++ daily_report/do_daily_reports.py
@@ -3,14 +3,16 @@ import os
 from daily_report.daily_report import *
 from daily_report.daily_report_v2 import *
 from daily_report.daily_report_v3 import *
+from daily_report.daily_report_v4 import *
 
 
-def do_daily_reports(version=3, send_slack=False):
+def do_daily_reports(version=4, send_slack=False):
     time_start = time.time()
     # print('Doing daily reports of: ' + animal)
     # folder = '/home/alexis/pv_nmdar_eranet/experiments/2AFC/setups/'
     # folder = '/home/alexis/pv_nmdar_eranet/experiments/2AFC_2/setups/'
-    folder = '/home/alexis/pv_nmdar_eranet/experiments/2AFC_3/setups/'
+    # folder = '/home/alexis/pv_nmdar_eranet/experiments/2AFC_3/setups/'
+    folder = '/home/alexis/pv_nmdar_eranet/experiments/2AFC_4/setups/'
     # animal = input('Enter animal')
     animals = os.listdir(folder)
     animals.sort()
@@ -26,87 +28,7 @@ def do_daily_reports(version=3, send_slack=False):
 
     # # Remove animals not training
     try:
-        animals.remove('387')
-    except ValueError:
-        pass
-
-    try:
-        animals.remove('395')
-    except ValueError:
-        pass
-
-    try:
-        animals.remove('398')
-    except ValueError:
-        pass
-
-    try:
-        animals.remove('419')
-    except ValueError:
-        pass
-
-    try:
-        animals.remove('420')
-    except ValueError:
-        pass
-
-    # try:
-    #     animals.remove('422')
-    # except ValueError:
-    #     pass
-
-    try:
-        animals.remove('501')
-    except ValueError:
-        pass
-
-    try:
-        animals.remove('614')
-    except ValueError:
-        pass
-
-    try:
-        animals.remove('615')
-    except ValueError:
-        pass
-    #
-    # try:
-    #     animals.remove('616')
-    # except ValueError:
-    #     pass
-
-    try:
-        animals.remove('617')
-    except ValueError:
-        pass
-
-    try:
-        animals.remove('618')
-    except ValueError:
-        pass
-
-    try:
-        animals.remove('619')
-    except ValueError:
-        pass
-
-    try:
-        animals.remove('620')
-    except ValueError:
-        pass
-
-    # try:
-    #     animals.remove('623')
-    # except ValueError:
-    #     pass
-
-    try:
-        animals.remove('625')
-    except ValueError:
-        pass
-
-    try:
-        animals.remove('627')
+        animals.remove('808')
     except ValueError:
         pass
 
@@ -127,7 +49,7 @@ def do_daily_reports(version=3, send_slack=False):
         if len(split_sessions) > 1:
             for j in range(len(split_sessions)):
                 path = folder2 + split_sessions[j] + '/' + split_sessions[
-                    j] + '.csv'  # Get csv file path to input parse/parse_v2.py
+                    j] + '.csv'  # Get csv file path to input parse/parse_vX.py
                 print(""'Doing the daily report(s) of animal ', animals[i], ': ', len(split_sessions),
                       ' sessions found in the same date(s)'"", sep='')
                 # print(path)
@@ -138,7 +60,10 @@ def do_daily_reports(version=3, send_slack=False):
                     # daily_report_v2(path, send_slack=send_slack)
                     pass
                 elif version == 3:
-                    daily_report_v3(path, send_slack=send_slack)
+                    # daily_report_v3(path, send_slack=send_slack)
+                    pass
+                elif version == 4:
+                    daily_report_v4(path, send_slack=send_slack)
         else:
             path = folder2 + sessionID + '/' + sessionID + '.csv'  # Get csv file path to input parse/parse_v2.py
             print(""'Doing the daily report(s) of animal ', animals[i], ': ', len(split_sessions),
@@ -152,7 +77,10 @@ def do_daily_reports(version=3, send_slack=False):
                 # daily_report_v2(path, send_slack=send_slack)
                 pass
             elif version == 3:
-                daily_report_v3(path, send_slack=send_slack)
+                # daily_report_v3(path, send_slack=send_slack)
+                pass
+            elif version == 4:
+                daily_report_v4(path, send_slack=send_slack)
 
     time_end = time.time()
     runtime = time_end - time_start
