3b5cc889c22d3516141860f35c54840aba5dcb7b
Alexis <alexis.cervan@gmail.com>
2023-03-31 16:55:32 +0200

    Added PyBpod VAR_BLOCK_LEN

--- parse/parse_v2.py
+++ parse/parse_v2.py
@@ -81,6 +81,12 @@ def parse_v2(path):
     except IndexError:
         delay = [np.nan] * n_trials
 
+    try:
+        block_len = [float(df[df.MSG == 'VAR_BLOCK_LEN']['+INFO'].iloc[0])] * n_trials  # Added on 31-03-2023
+    except IndexError:
+        block_len = [np.nan] * n_trials
+
+
     # Registered values (out of loop)
     reward_side = df[df.MSG == 'REWARD_SIDE']['+INFO'].iloc[-1]  # [-1] to take the last one in case CB was on
 
@@ -360,17 +366,18 @@ def parse_v2(path):
                'RespWinStart', 'RespWinEnd', 'RespWinLen', 'Filename', 'Filename2', 'FilesMatch', 'Message',
                'MessageFound', 'SoundLeft', 'SoundRight', 'Sound', 'ILD', 'ILDRep', 'Port1In', 'Port1Out', 'Port2In',
                'Port2Out', 'ValveLeft', 'ValveRight', 'AW', 'Switch', 'Timeout', 'Fixation', 'Stage', 'Motor', 'REC',
-               'Progression', 'CB', 'RespWin', 'ITI', 'WarmUp', 'RecoveryMode', 'P', 'PRight', 'Blocks', 'StimDur',
-               'Delay', 'SerialPort', 'Protocol', 'Creator', 'Project', 'Experiment', 'Board', 'Setup', 'NetPort',
-               'Subject', 'BpodApiVersion', 'Session', 'Date', 'SessionStart', 'SessionEnd']
+               'Progression', 'CB', 'RespWin', 'ITI', 'WarmUp', 'RecoveryMode', 'P', 'PRight', 'Blocks', 'BlockLen',
+               'StimDur', 'Delay', 'SerialPort', 'Protocol', 'Creator', 'Project', 'Experiment', 'Board', 'Setup',
+               'NetPort', 'Subject', 'BpodApiVersion', 'Session', 'Date', 'SessionStart', 'SessionEnd']
 
     data = list(zip(trial, reward_side, rep_trial, reward, punish, miss, wrong_lick, hit, after_hit, choice, rep_choice,
                     response, trial_start, trial_end, trial_len, stim_start, stim_end, stim_len, resp_win_start,
                     resp_win_end, resp_win_len, filename, filename2, files_match, message, message_found, sound_left,
                     sound_right, sound, ild, ild_rep, port1in, port1out, port2in, port2out, valve_1, valve_2, aw,
                     switch, timeout, fixation, stage, motor, rec, progression, cb, resp_win, iti, warm_up, recovery_mode,
-                    p, p_right, blocks, stim_dur, delay, serial_port, protocol, creator, project, experiment, board,
-                    setup, net_port, subject, bpod_api_version, session, date, time_session_started, time_session_ended))
+                    p, p_right, blocks, block_len, stim_dur, delay, serial_port, protocol, creator, project, experiment,
+                    board, setup, net_port, subject, bpod_api_version, session, date, time_session_started,
+                    time_session_ended))
 
     df_session = pd.DataFrame(data=data, columns=columns)
 
