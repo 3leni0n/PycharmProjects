5897977c7fd0c033522601570c4e3f3d33b70cc4
setup user <fake@lol.xyz>
2023-10-11 17:43:12 +0200

    Changed animals to be removed in do_daily_reports

--- daily_report/do_daily_reports.py
+++ daily_report/do_daily_reports.py
@@ -57,19 +57,19 @@ def do_daily_reports(version=4, experiment='2AFC_4', index=-1, send_slack=False)
     username = os.path.split(os.path.expanduser('~'))[-1]
 
     if username == 'setup1':
-        remove_from_setup1 = ['876', '973', '975']
+        remove_from_setup1 = []
     else:
         remove_from_setup1 = []
 
     # Switched to training in setup1, remove from setup2 to avoid doing reports from old sessions
     if username == 'setup2':
-        remove_from_setup2 = ['907', '911']
+        remove_from_setup2 = ['873', '875', '907', '911']
     else:
         remove_from_setup2 = []
 
     # Switched to training in setup1, remove from setup2 to avoid doing reports from old sessions
     if username == 'delarocha3':
-        remove_from_setup_ephys = ['420', '422', '619', '623', '876', '909']
+        remove_from_setup_ephys = ['420', '422', '619', '623', '876']
     else:
         remove_from_setup_ephys = []
 
