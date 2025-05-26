811770763d2fe760e444f851a2444689bab4b0c5
3leni0n <alexis.cervan@gmail.com>
2025-05-26 19:03:25 +0200

    Modified the close cam function of task_tools

--- task_tools/task_tools.py
+++ task_tools/task_tools.py
@@ -113,9 +113,6 @@ def open_cam(n_cams=1, cam_sync=None, states_on_sync=False, mask=False):
     if not os.path.exists(os.path.expanduser(video_folder)):
         os.makedirs(os.path.expanduser(video_folder))
 
-    # Start  video
-    no_cam = False  # So the code doesn't open the cam twice, probably it will work without it, try
-
     # Select automatically the cam depending on the PC (only for n_cams=1)
     username = os.getlogin()
     if username == 'setup0':
@@ -137,14 +134,12 @@ def open_cam(n_cams=1, cam_sync=None, states_on_sync=False, mask=False):
                          states_on_sync=states_on_sync,
                          mask=mask
                          )
-            cam_OK = False   # Needed???? Commented on 01/03/2023
             cam.play()
             if int(conf.VAR_REC) > 0:
                 cam.record()
         except:
             print(
                 "Could not open device. This may happen because either it's already in use or wrong device index number was provided")
-            no_cam = True
 
     elif n_cams == 2:  # For when 2 cams are needed (ephys box only, 'cam_double' and 'stage_training' tasks)
         try:
@@ -170,7 +165,6 @@ def open_cam(n_cams=1, cam_sync=None, states_on_sync=False, mask=False):
                           states_on_sync=states_on_sync,
                           mask=mask
                           )
-            cam_OK = False  # Needed???? Commented on 01/03/2023
             cam1.play()
             cam2.play()
             if int(conf.VAR_REC) > 0:
@@ -179,7 +173,6 @@ def open_cam(n_cams=1, cam_sync=None, states_on_sync=False, mask=False):
         except:
             print(
                 "Could not open device. This may happen because either it's already in use or wrong device index number was provided")
-            no_cam = True
 
 
 def close_cam(n_cams=1):
@@ -189,10 +182,8 @@ def close_cam(n_cams=1):
     """
 
     if n_cams == 1:  # For when a 1 cam is needed (all the test tasks, boxes 1-8)
-        if not no_cam:
-            cam.stop()
+        cam.stop()
 
     elif n_cams == 2:  # For when 2 cams are needed (ephys box only, 'cam_double' and 'stage_training' tasks)
-        if not no_cam:
-            cam1.stop()
-            cam2.stop()
+        cam1.stop()
+        cam2.stop()
