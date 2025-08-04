f79151fa3483462b1d58a6c382b118a5544e2631
Alexis <alexis.cervan@gmail.com>
2025-08-04 19:04:27 +0200

    Polished glue sessions codes to prescind of protocol var and run it over all previous groups. Created Thesis notebook

--- glue_sessions/glue_sessions.py
+++ glue_sessions/glue_sessions.py
@@ -23,8 +23,6 @@ def glue_sessions(animal=None, protocol='stage_training_v6', experiment='2AFC_6'
     :return: pandas DataFrame with the data, .csv file with the ID of the corrupted sessions
     """
 
-    time_start = time.time()
-
     if experiment is None:
 
         folder_in = Path.home() / 'pv_nmdar_eranet' / 'experiments'  # Where the data for all animals is
@@ -115,7 +113,8 @@ def glue_sessions(animal=None, protocol='stage_training_v6', experiment='2AFC_6'
             try:
                 if protocol == 'stage_training':
                     df_session = parse(path)  # Parse session
-                elif protocol == 'stage_training_v2' or 'stage_training_v3' or 'stage_training_v4':
+                elif (protocol == 'stage_training_v2' or 'stage_training_v3' or 'stage_training_v4' or 'stage_training_v5'
+                      or 'stage_training_v6'):
                     df_session = parse_v2(path)  # Parse session v2
                 df = pd.concat([df, df_session])  # Add parsed session to the bottom of the DataFrame
             except (IndexError, ValueError, FileNotFoundError, ZeroDivisionError):  # When passing 2 exceptions it must be in this syntax
@@ -140,22 +139,16 @@ def glue_sessions(animal=None, protocol='stage_training_v6', experiment='2AFC_6'
             wr = csv.writer(f)
             wr.writerow(corrupted_sessions)
 
-    time_end = time.time()
-    runtime = time_end - time_start
-    print('The script took', round(runtime, 2), 'seconds to run')
-
     return df, corrupted_sessions
 
 
-def update_glued_sessions(protocol='stage_training_v6', experiment='2AFC_6'):
+def update_glued_sessions(experiment='2AFC_6'):
     """
     Update the glued_sessions .csv files for all animals with the non yet included sessions.
     :param protocol: task code version
     :param experiment: batch of the animals
     :return:
     """
-
-    time_start = time.time()
 
     if experiment is None:
 
@@ -172,6 +165,8 @@ def update_glued_sessions(protocol='stage_training_v6', experiment='2AFC_6'):
 
         print('Experiments: ' + str(experiments)[1:-1])  # Remove square brackets
         experiment = input('Enter experiment name')
+
+    protocol = 'stage_training_v' + experiment[-1]  # Get the last digit of the experiment
 
     folder = Path.home() / 'pv_nmdar_eranet' / 'experiments' / experiment / 'setups'  # Where the data for all animals is
     animals = os.listdir(folder)  # List animals
@@ -190,12 +185,8 @@ def update_glued_sessions(protocol='stage_training_v6', experiment='2AFC_6'):
         print(f'Updating sessions of animal {animals[i]}...')
         glue_sessions(animal=animals[i], protocol=protocol, experiment=experiment, to_csv=True)
 
-    time_end = time.time()
-    runtime = time_end - time_start
-    print('The script took', round(runtime, 2), 'seconds to run')
 
-
-def glue_animals(protocol='stage_training_v4', experiment='2AFC_4', update=False, to_csv=False):
+def glue_animals(experiment='2AFC_6', update=True, to_csv=False):
     """
     Glue all the sessions from all the animals of a given batch.
     :param protocol: task code version
@@ -205,16 +196,14 @@ def glue_animals(protocol='stage_training_v4', experiment='2AFC_4', update=False
     :return: pandas DataFrame with the data
     """
 
-    time_start = time.time()
-
     # Get the path to the data
-    experiment, folder = get_experiment(experiment, session='glue_sessions')
+    experiment, folder = get_experiment(experiment, path_session='glue_sessions')
 
-    # folder_in = Path.home() / 'PycharmProjects' / 'glue_sessions' / experiment  # Where the data for all animals is
+    protocol = 'stage_training_v' + experiment[-1]  # Get the last digit of the experiment
 
     # Update first the glued sessions
     if update:
-        update_glued_sessions(protocol=protocol, experiment=experiment)  # Update glued sessions first
+        update_glued_sessions(experiment=experiment)  # Update glued sessions first
 
     animals = os.listdir(folder)  # List animals
     animals.sort()  # Sort them by name
@@ -228,34 +217,24 @@ def glue_animals(protocol='stage_training_v4', experiment='2AFC_4', update=False
     df.reset_index(drop=True, inplace=True)
 
     if to_csv:
-        filename = experiment + '_all_sessions'
+        filename = experiment + '_glued_sessions'
         df.to_csv(Path(folder / filename).with_suffix('.csv'), index=False)  # index=False to avoid the 'Unnamed: 0' column
-
-    time_end = time.time()
-    runtime = time_end - time_start
-    print('The script took', round(runtime, 2), 'seconds to run')
 
     return df
 
 
-def glue_batches(batches=['2AFC_2', '2AFC_3', '2AFC_4']):
+def glue_groups(groups=['2AFC_2', '2AFC_3', '2AFC_4', '2AFC_5', '2AFC_6']):
     """
-    Glue all sessions from all animals from all batches.
-    :param batches: Batches of animals to glue together
+    Glue all sessions from all animals from all groups.
+    :param groups: Batches of animals to glue together
     :return: pandas Dataframe with the data
     """
 
-    time_start = time.time()
-
     df = pd.DataFrame()
 
-    for i in range(len(batches)):
-        df_batch = glue_animals(experiment=batches[i], to_csv=False)
-        df = pd.concat([df, df_batch])  # Add parsed session to the bottom of the DataFrame
+    for i in range(len(groups)):
+        df_groups = glue_animals(experiment=groups[i], update=False, to_csv=False)
+        df = pd.concat([df, df_groups])  # Add parsed session to the bottom of the DataFrame
     df.reset_index(drop=True, inplace=True)
-
-    time_end = time.time()
-    runtime = time_end - time_start
-    print('The script took', round(runtime, 2), 'seconds to run')
 
     return df
